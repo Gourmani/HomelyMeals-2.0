8fc6d6b43da637500765388e7cc16d346246a395
Gourmani <gmchy31@gmail.com>
2026-03-20 14:23:03 +0530

    Email Issue Fix

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,37 +1,22 @@
-const nodemailer = require("nodemailer")
-const dns=require("dns")
-dns.setDefaultResultOrder("ipv4first") // avoid IPv6 issues on some platforms (e.g. Render)
+const sgMail = require("@sendgrid/mail")
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 const sendEmail = async (to, subject, text) => {
-
   console.log("SENDING EMAIL TO:", to)
 
-  const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false, // IMPORTANT for Render
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS
-    },
-    tls: {
-      rejectUnauthorized: false
-    },
-    connectionTimeout: 10000 // avoid 30s freeze
-  })
+  const msg = {
+    to,
+    from: process.env.EMAIL_USER, // must be verified sender
+    subject,
+    text,
+  }
 
   try {
-    const info = await transporter.sendMail({
-      from: `"Homely Meals" <${process.env.EMAIL_USER}>`,
-      to,
-      subject,
-      text
-    })
-
-    console.log("EMAIL SENT:", info.response)
-
+    await sgMail.send(msg)
+    console.log("EMAIL SENT SUCCESS")
   } catch (error) {
-    console.log("EMAIL ERROR FULL:", error.message)
+    console.log("EMAIL ERROR FULL:", error.response?.body || error.message)
     throw error
   }
 }
